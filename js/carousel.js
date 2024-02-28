3168d985d5a26eb6d3c817299293bf54e3ad80fe
nhatlonh <longcpnse183738@fpt.edu.vn>
2024-02-28 18:52:23 +0700

    Update script carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -3,3 +3,10 @@ $(".single-carousel").slick({
     autoplaySpeed: 2000,
     arrows: false,
 });
+
+$('.product-carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+});

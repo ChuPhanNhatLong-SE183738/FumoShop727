97a75d9637699c1dfa1567f3b4a0cf01be85b219
nhatlonh <longcpnse183738@fpt.edu.vn>
2024-03-01 19:02:58 +0700

    Update login icon

--- js/carousel.js
+++ js/carousel.js
@@ -8,7 +8,9 @@ $('.product-carousel').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    arrows: false,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
 });
